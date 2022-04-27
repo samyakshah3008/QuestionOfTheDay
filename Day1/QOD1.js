c1ac967d4dff0ecafe0e47c11d9b1782caf2497f
Samyak Shah <samyakshah3008@gmail.com>
2022-04-27 15:20:07 +0530

    day1 question JS integrated

---
+++ Day1/QOD1.js
@@ -0,0 +1,12 @@
+// method 1
+const numToPower = (a, b) => {
+  const ans = Math.pow(a, b);
+  console.log(ans);
+};
+
+numToPower(2, 5);
+
+// method 2
+
+const numToPower2 = (c, d) => c ** d;
+console.log(numToPower2(2, 5));
